8fb311a958347fe1e855fcd65ae1818707541cff
Prateek Chhetri <pchhetri@zendesk.com>
2016-11-12 21:50:40 -0600

    Update homepage to work with new components

--- pages/home/index.js
+++ pages/home/index.js
@@ -8,27 +8,79 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import React, { PropTypes } from 'react';
+import React from 'react';
 import Layout from '../../components/Layout';
 import s from './styles.css';
-import { title, html } from './index.md';
+import {title, html} from './index.md';
 import Welcome from '../../components/Welcome'
+import Loader from '../../components/Loader'
+import NewRequest from '../../components/NewRequest'
+import {fb} from '../../core/firebaseApi'
+import RequestConfirmation from '../../components/RequestConfirmation'
 
 class HomePage extends React.Component {
-
-  static propTypes = {
-    articles: PropTypes.array.isRequired,
-  };
 
   componentDidMount() {
     document.title = title;
   }
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      showHome: true,
+      showLoader: null,
+      showRequest: null,
+      showConfirmation: null,
+      room: null
+    }
+  }
+
+  handleCode(code, id)
+  {
+    console.log('Room code entered was: ' + code);
+    this.setState({showHome: false, showLoader: true});
+    const db = fb.database();
+    const ref = db.ref();
+    const roomsRef = ref.child("rooms");
+    const self = this;
+    roomsRef.orderByChild("code").equalTo(123).on("child_added", function(snapshot) {
+      console.log(snapshot.val());
+      self.setState({
+        room: snapshot.val(),
+        showLoader: false,
+        showRequest: true
+      })
+    });
+  }
+
+  handleNewRequest()
+  {
+    // Todo: Send request info to firebase
+    this.setState({showRequest:false})
+    this.setState({showConfirmation: true});
+  }
+
   render() {
+    const welcomePage = this.state.showHome
+    const loader = this.state.showLoader
+    const requestPage = this.state.showRequest
+    const confirmationPage = this.state.showConfirmation
+
     return (
       <Layout className={s.content}>
-        <div dangerouslySetInnerHTML={{ __html: html }} />
-        <Welcome />
+        {/* <div dangerouslySetInnerHTML={{ __html: html }} /> */}
+        {welcomePage
+          ? <Welcome onCode={this.handleCode.bind(this)}/>
+          : null}
+        {loader
+          ? <Loader/>
+          : null}
+        {requestPage
+          ? <NewRequest room={this.state.room} onRequest={this.handleNewRequest.bind(this)}/>
+          : null}
+        {confirmationPage
+          ? <RequestConfirmation/>
+          : null}
       </Layout>
     );
   }
