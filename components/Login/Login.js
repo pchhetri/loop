4b0e7a98885efe301ca199ab4223ccef278114ab
jpandl19 <jpandl19@gmail.com>
2016-12-18 15:41:48 -0600

    Removed unecessary comments 💬💬

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -4,26 +4,6 @@ import history from '../../core/history'
 import FontAwesome from 'react-fontawesome'
 import { LOGIN_INIT, LOGIN_WORKING, LOGIN_OK } from '../../constants'
 
-  // var working = false;
-  // $('.login').on('submit', function(e) {
-  //   e.preventDefault();
-  //   if (working) return;
-  //   working = true;
-  //   var $this = $(this),
-  //     $state = $this.find('button > .state');
-  //   $this.addClass('loading');
-  //   $state.html('Authenticating');
-  //   setTimeout(function() {
-  //     $this.addClass('ok');
-  //     $state.html('Welcome back!');
-  //     setTimeout(function() {
-  //       $state.html('Log in');
-  //       $this.removeClass('ok loading');
-  //       working = false;
-  //     }, 4000);
-  //   }, 3000);
-  // });
-
 const Login = ({status, error, fields, handleChange, handleSubmit}) =>  {
   const { email, password } = fields
   const { spinnerClass, buttonClass, statusText } = getClassByStatus( status )
